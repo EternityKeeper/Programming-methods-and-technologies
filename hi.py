f36c404d8f05ba2f3934e668cd7995be8a579779
EternityKeeper <90920397+EternityKeeper@users.noreply.github.com>
2025-10-07 18:14:14 +0300

    normal task 2

--- hi.py
+++ hi.py
@@ -15,7 +15,29 @@ print(find_min(numbers))
 
 #normal task 2
 
-n = int(input("Введите число: "))
-print(f"Таблица умножения для числа {n}:")
+number = int(input("Введите число: "))
+print(f"Таблица умножения для числа {number}:")
 for i in range(1, 11):
-    print(f"{n} × {i} = {n * i}")
+    print(f"{number} × {i} = {number * i}")
+
+#normal task 3
+
+number_a = float(input("Введите первое число: "))
+operation = input("Введите операцию (+, -, *, /): ")
+number_b = float(input("Введите второе число: "))
+
+if operation == '+':
+    result = number_a + number_b
+elif operation == '-':
+    result = number_a - number_b
+elif operation == '*':
+    result = number_a * number_b
+elif operation == '/':
+    if number_b != 0:
+        result = number_a / number_b
+    else:
+        result = "Ошибка: деление на ноль!"
+else:
+    result = "Ошибка: неизвестная операция!"
+
+print(f"Результат: {number_a} {operation} {number_b} = {result}")
