8e5595065e9615b04b98aa1e7e3aeadc4a15fe61
EternityKeeper <90920397+EternityKeeper@users.noreply.github.com>
2025-10-07 18:06:12 +0300

    normal task 1

--- hi.py
+++ hi.py
@@ -1,3 +1,15 @@
-print("Hello world!")
-print("hi")
-print("3")
+#normal task 1
+def find_min(lst):
+    if not lst:
+        return None
+
+    min_val = lst[0]
+    for num in lst[1:]:
+        if num < min_val:
+            min_val = num
+    return min_val
+
+
+numbers = [5, -2, 10, 3, -8, 0]
+print(find_min(numbers))
+
